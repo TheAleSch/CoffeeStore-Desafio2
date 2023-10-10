52fb83863007a0178763117c43c060eb51604586
Alexandre Schrammel <alexandre.schrammel@gmail.com>
2023-10-10 00:42:37 -0300

    router added

--- CoffeeStore/src/App.tsx
+++ CoffeeStore/src/App.tsx
@@ -3,12 +3,17 @@ import { Button } from "./components/Button";
 import { defaultTheme } from "../src/styles/themes/default";
 import { GlobalStyle } from "./styles/globals";
 import { Header } from "./components/Header";
+import { Router } from "./components/router";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <Header />
-      <GlobalStyle />
+      <BrowserRouter>
+        <Header />
+        <Router />
+        <GlobalStyle />
+      </BrowserRouter>
     </ThemeProvider>
   );
 }
